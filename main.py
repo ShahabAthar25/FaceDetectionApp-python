2b79a4cc1f5998fd4813cb96dfc0406667a5b765
ShahabAthar25 <shahabathar25@gmail.com>
2021-05-16 17:40:19 +0500

    Add pretrained Data

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import cv2
 
-import tkinter
+# loading pre trained data here
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# choosing face to detect on
+img = cv2.imread('RDJ.jpg')
