db79740cb12f1a398ca74b51d59f38f2633d636a
ShahabAthar25 <shahabathar25@gmail.com>
2021-05-16 18:02:26 +0500

    Add Face Detection

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # choosing face to detect on
 img = cv2.imread('RDJ.jpg')
 
-cv2.imshow('My Face: ', img)
+# Turning Img To Grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+print(face_cordinates)
+
+# Showing Image
+cv2.imshow('My Face: ', grayscaled_img)
 
 # listening for key press
 cv2.waitKey()
