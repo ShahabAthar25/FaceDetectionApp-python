a2fe643077d9a3a72faa8252d4616aace446ca7a
ShahabAthar25 <shahabathar25@gmail.com>
2021-05-16 18:32:51 +0500

    Add rectangle around face

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+from random import randrange
 
 # loading pre trained data here
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -9,12 +10,17 @@ img = cv2.imread('RDJ.jpg')
 # Turning Img To Grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Getting Face Cords
 face_cordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 print(face_cordinates)
 
+# Adding Rectangle On Faces
+for (x, y, w, h) in face_cordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(255), randrange(255), randrange(255)), 2)
+
 # Showing Image
-cv2.imshow('My Face: ', grayscaled_img)
+cv2.imshow('My Face: ', img)
 
 # listening for key press
 cv2.waitKey()
