dbda39fe4cef9cb7be72c221f4795d6f904dd853
ShahabAthar25 <shahabathar25@gmail.com>
2021-05-16 17:46:12 +0500

    Adding a gui to show pic

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # choosing face to detect on
 img = cv2.imread('RDJ.jpg')
+
+cv2.imshow('My Face: ', img)
+
+# listening for key press
+cv2.waitKey()
